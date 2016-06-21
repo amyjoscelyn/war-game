2e695b8f5def88e8267af65d6266e7ebfe466e3e
amyjoscelyn <amy.joscelyn@flatironschool.com>
2016-06-21 14:38:56 -0400

    Initial commit, Card class set up

---
+++ WarGame/WarGame/Card.swift
@@ -0,0 +1,57 @@
+//
+//  Card.swift
+//  WarGame
+//
+//  Created by Amy Joscelyn on 6/21/16.
+//  Copyright © 2016 Amy Joscelyn. All rights reserved.
+//
+
+import Foundation
+
+class Card
+{
+    class func validSuits() -> [String]
+    {
+        let spades = "♠️"
+        let hearts = "♥️"
+        let diamonds = "♦️"
+        let clubs = "♣️"
+        
+        return [spades, hearts, diamonds, clubs]
+    }
+    
+    class func validRanks() -> [String]
+    {
+        return ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    }
+    
+    let suit: String
+    let rank: String
+    let cardLabel: String
+    let cardValue: UInt
+    
+    var description: String
+    {
+        return self.cardLabel
+    }
+    
+    init(suit: String, rank: String)
+    {
+        self.suit = suit
+        self.rank = rank
+        self.cardLabel = "\(suit)\(rank)"
+        
+        if rank == "J" || rank == "Q" || rank == "K"
+        {
+            self.cardValue = 10
+        }
+        else if rank == "A"
+        {
+            self.cardValue = 1
+        }
+        else
+        {
+            self.cardValue = UInt(rank)!
+        }
+    }
+}
